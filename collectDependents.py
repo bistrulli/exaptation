71c71b5e529ad6de716e6b94bc86a6c3489977ef
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-09-22 10:54:03 +0200

    minor

---
+++ collectDependents.py
@@ -0,0 +1,31 @@
+import ijson
+import json
+import subprocess
+from pathlib import Path
+
+def collectDependents(repo):
+    return subprocess.check_output(["github-dependents-info","--json","--repo",repo])
+
+def isProcessed(repoOwner,repoName):
+    return Path('./dependents/%s_%s_deps.json'%(repoOwner,repoName)).is_file()
+
+def saveDependents(repoOwner,repoName,dep):
+    ofile=open('./dependents/%s_%s_deps.json'%(repoOwner,repoName),"w+")
+    ofile.write(json.dumps(json.loads(dep)))
+    ofile.flush()
+    ofile.close()
+
+
+
+DATA_PATH = '/Users/emilio-imt/Desktop/exaptation/repo_metadata.json'
+with open(DATA_PATH, "rb") as f:
+    for repo in ijson.items(f, "item"):
+        if(isProcessed(repo["owner"],repo["name"])):
+            continue
+        try:
+           dep=collectDependents(repo["nameWithOwner"])
+           saveDependents(repo["owner"],repo["name"],dep)
+        except Exception as e:
+            print(Exception, e)
+        finally:
+            break
