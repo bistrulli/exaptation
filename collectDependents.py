1907d32b67ac492764dbe4a906697bb2df7d6a70
Fabio Pinelli <fabiopinelli@gmail.com>
2023-10-16 10:22:49 +0000

    changed the paths for files

--- collectDependents.py
+++ collectDependents.py
@@ -9,17 +9,17 @@ def collectDependents(repo):
     return subprocess.check_output(["github-dependents-info","--json","--repo",repo],timeout=120)
 
 def isProcessed(repoOwner,repoName):
-    return Path('./dependents/%s_%s_deps.json'%(repoOwner,repoName)).is_file()
+    return Path('./dependents/dependents/%s_%s_deps.json'%(repoOwner,repoName)).is_file()
 
 def saveDependents(repoOwner,repoName,dep):
-    ofile=open('./dependents/%s_%s_deps.json'%(repoOwner,repoName),"w+")
+    ofile=open('./dependents/dependents/%s_%s_deps.json'%(repoOwner,repoName),"w+")
     ofile.write(json.dumps(json.loads(dep)))
     ofile.flush()
     ofile.close()
 
 
 
-DATA_PATH = './repo_metadata.json'
+DATA_PATH = '/home/fabio.pinelli/exaptation_fabio/repo_metadata.json'
 pbar = tqdm(total=3152515,miniters=1)
 with open(DATA_PATH, "rb") as f:
     for repo in ijson.items(f, "item"):
