51ced6de39f0fbcd1ceaad87cc29a85f7499917e
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2024-07-01 23:41:06 +0200

    bug fix asynch gemini

--- geminiTopic.py
+++ geminiTopic.py
@@ -22,9 +22,9 @@ def initApi():
 	genai.configure(api_key=GOOGLE_API_KEY)
 
 def parseTopic(rawtopics):
-	rawtopics = "".join([part.text for part in rawtopics.candidates[0].content.parts])
 	topics=[]
 	try:
+		rawtopics = "".join([part.text for part in rawtopics.candidates[0].content.parts])
 		topics=json.loads(re.findall(r"\{.*\}",rawtopics)[0])["main_topics"]
 	except Exception as e:
 		pass
