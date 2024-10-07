a0a3fad74c7632eb733a03c97fb1900d93a4f5b9
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2024-10-07 12:52:54 +0200

    log management

--- getGitReadme.py
+++ getGitReadme.py
@@ -14,11 +14,15 @@ def getReadme(owner,repo,outdir="./Readme",token=None):
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        readme_data = response.json()
-        readme_content = base64.b64decode(readme_data['content']).decode('utf-8')
-        p_file=Path(f"{outdir}/{owner}_{repo}.md")
-        p_file.parent.mkdir(parents=True, exist_ok=True)
-        p_file.write_text(readme_content, encoding ="utf-8")
+        try:
+            readme_data = response.json()
+            readme_content = base64.b64decode(readme_data['content']).decode('utf-8')
+            p_file=Path(f"{outdir}/{owner}_{repo}.md")
+            p_file.parent.mkdir(parents=True, exist_ok=True)
+            p_file.write_text(readme_content, encoding ="utf-8")
+        except Exception as e:
+            print(f"Failed to fetch https://api.github.com/repos/{owner}/{repo}/readme: {response.status_code} ")
+            print(e)
     else:
         print(f"Failed to fetch https://api.github.com/repos/{owner}/{repo}/readme: {response.status_code} ")
 
