d6f4b828194b7070b5813e9e5f6e8005715c708d
emilio-imt <emilio-imt@MacBook-Pro-M1.local>
2024-11-18 08:52:58 +0100

    refined get embedding

--- geminiTopic.py
+++ geminiTopic.py
@@ -81,7 +81,7 @@ def getTopicEmbedding(repo=None):
 	# Your list of words
 	try:
 		words=["no-tags"]
-		if(repo["topics"] is not None and not repo["topics"].isna()):
+		if(repo["topics"] is not None and type(repo["topics"])==str):
 			words = repo["topics"].split(",")
 
 		# Generate embeddings for the list of words
